953d2ddb382d824b75bcc08141fd61971d9747cd
XEnterNameX <44647396+XEnterNameX@users.noreply.github.com>
2018-12-06 18:30:27 +0100

    Controller-Changes (throws Exception)

--- STLParser/src/controller/Controller.java
+++ STLParser/src/controller/Controller.java
@@ -3,72 +3,105 @@ package controller;
 import Metamodell.*;
 import Metamodell.impl.AreaImpl;
 import Metamodell.impl.MetamodellFactoryImpl;
-
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
+import java.util.HashMap;
+import java.util.List;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class Controller<E> {
+public class Controller
+{
 	
-	
-	public void computeDegree(Edge e) {
-		EList facets = e.getF();
-		if(facets.size() == 2) {
+	public void computeDegree(Edge e)
+	{
+		System.out.println("\t" + this.getClass().getSimpleName() + ".computeDegree(Edge" + e + ")");
+		EList<Facet> facets = e.getF();
+		if (facets.size() == 2)
+		{
 			Facet f1 = (Facet) facets.get(0);
 			Facet f2 = (Facet) facets.get(1);
-			
-			double scalar = f1.getNormal().getX() * f2.getNormal().getX() + 
-							f1.getNormal().getY() * f2.getNormal().getY() + 
-							f1.getNormal().getZ() * f2.getNormal().getZ();
-			double mult =  Math.sqrt(f1.getNormal().getX()*f1.getNormal().getX() +
-						   f1.getNormal().getY()*f1.getNormal().getY() +
-						   f1.getNormal().getZ()*f1.getNormal().getZ())*
-						   Math.sqrt(f2.getNormal().getX()*f2.getNormal().getX() +
-						   f2.getNormal().getY()*f2.getNormal().getY() +
-						   f2.getNormal().getZ()*f2.getNormal().getZ());
-							
-			
-			e.setDegree(Math.acos(scalar/mult));
+			double scalar = f1.getNormal().getX() * f2.getNormal().getX() + f1.getNormal().getY() * f2.getNormal().getY() + f1.getNormal().getZ() * f2.getNormal().getZ();
+			double mult = Math.sqrt(f1.getNormal().getX() * f1.getNormal().getX() + f1.getNormal().getY() * f1.getNormal().getY() + f1.getNormal().getZ() * f1.getNormal().getZ()) * Math.sqrt(f2.getNormal().getX() * f2.getNormal().getX() + f2.getNormal().getY() * f2.getNormal().getY() + f2.getNormal().getZ() * f2.getNormal().getZ());
+			e.setDegree((float) Math.acos(scalar / mult));
 		}
-		
-		
-		
 	}
 	
-	public void createAreas(Solid s) {
-		
-		Queue<Facet> q = (Queue<Facet>) s.getFacets();
-		Queue<Facet> queue = new LinkedList<Facet>();
-		ArrayList<Area> areas = new ArrayList<Area>();
-		for(Facet f: q)
+	public void createAreas(Solid s)
+	{
+		EList<Facet> q = s.getFacets();
+		HashMap<EqVec, EList<Facet>> areas = new HashMap<EqVec, EList<Facet>>();
+		// Go through all Facet-Objects
+		for (Facet f : q)
 		{
-			AreaImpl a = (AreaImpl) MetamodellFactoryImpl.init().createArea();
-			q.remove(f);
-			queue.add(f);
-			for(Facet fac : queue) 
-			{	
-				a.eSet(0, fac);
-				for(Edge e: fac.getEdges()) 
-				{
-					if (e.getDegree() == 0) 
-					{
-						queue.add(e.getF().get(0));
-						queue.add(e.getF().get(0));
-						
-					}
-					
-				}
-				
-			
+			// Get the normal
+			Vector3f n = f.getNormal();
+			// Convert to Vector3f "implementation" that overrides the equals(Object)-method
+			EqVec ev = new EqVec(n);
+			EList<Facet> list = areas.get(ev);
+			// No Area found
+			if (list == null)
+			{
+				list = new BasicEList<Facet>();
+				areas.put(ev, list);
 			}
-			queue.clear();
-			
+			// Put into area
+			list.add(f);
+		}
+		EList<Area> result = new BasicEList<Area>();
+		for (List<Facet> list : areas.values())
+		{
+			// Create Area for each list in HashMap
+			// System.out.println("Controller.createAreas(Solid): created Area");
+			AreaImpl a = (AreaImpl) MetamodellFactoryImpl.init().createArea();
+			a.eSet(MetamodellPackage.AREA__FACETS, list);
+			result.add(a);
+			for(Facet f : list)
+			{
+				f.setArea(a);
+			}
+		}
+//		s.eSet(MetamodellPackage.SOLID__AREA, result);
+		System.out.println(getClass().getSimpleName() + ".createAreas(Solid " + s + ")");
+		System.out.println("\tcreated " + result.size() + " Area-Objects");
+	}
+	
+	private static class EqVec
+	{
+		private final Vector3f v;
+		
+		public EqVec(final Vector3f v)
+		{
+			float l = (float) Math.sqrt((v.getX() * v.getX()) + (v.getY() * v.getY()) + (v.getZ() * v.getZ()));
+			if (l != 0)
+			{
+				this.v = MetamodellFactoryImpl.init().createVector3f();
+				v.setX(v.getX() / l);
+				v.setY(v.getY() / l);
+				v.setZ(v.getZ() / l);
+			}
+			else
+			{
+				this.v = MetamodellFactoryImpl.init().createVector3f();
+				v.setX(0);
+				v.setY(0);
+				v.setZ(0);
+			}
+		}
+		
+		private boolean eq(final Vector3f ov)
+		{
+			return v.getX() == ov.getX() && v.getY() == ov.getY() && v.getZ() == ov.getZ();
+		}
+		
+		@Override
+		public boolean equals(Object o)
+		{
+			if (o instanceof EqVec)
+			{
+				EqVec eqv = (EqVec) o;
+				return eq(eqv.v);
+			}
+			return false;
 		}
 	}
 	
-
 }
-
-
--- STLParser/src/parser/Parser.java
+++ STLParser/src/parser/Parser.java
@@ -9,71 +9,78 @@ import org.emoflon.ibex.tgg.run.stlimportexport.SYNC_App;
 import org.xtext.example.mydsl.api.RunParser;
 import org.xtext.example.mydsl.myDsl.Solid;
 
-public class Parser {
+import Metamodell.Edge;
+import Metamodell.Facet;
+import Metamodell.impl.SolidImpl;
+import controller.Controller;
 
+public class Parser
+{
+	
 	private static SYNC sync;
 	
-	private static void initialiseFwdSynchroniser() throws IOException {
+	private static void initialiseFwdSynchroniser() throws IOException
+	{
 		if (sync != null)
 			sync.terminate();
-
+		
 		sync = new SYNC_App();
 		sync.setUpdatePolicy(matches -> {
-			for(IMatch m: matches.getMatches()) {
-				if(m.getRuleName().equals("SolidRule"))
+			for (IMatch m : matches.getMatches())
+			{
+				if (m.getRuleName().equals("SolidRule"))
 					
 					return m;
 				
-				if(m.getRuleName().equals("EdgeRule2")) {
-					System.out.println(m);
-					return m;
-				}
-				
-							
-				if(m.getRuleName().equals("VectorRule2"))
+				if (m.getRuleName().equals("EdgeRule2"))
 				{
 					System.out.println(m);
 					return m;
 				}
 				
-			
-				
+				if (m.getRuleName().equals("VectorRule2"))
+				{
+					System.out.println(m);
+					return m;
+				}
 				
 			}
-		return matches.getNext();	
+			return matches.getNext();
 		});
 	}
-
-	private void initialiseBwdSynchroniser() throws IOException {
+	
+	private void initialiseBwdSynchroniser() throws IOException
+	{
 		if (sync == null)
 			sync = new SYNC_App();
 	}
-
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws IOException
+	{
 		RunParser stlParser = new RunParser("test.stl");
 		Optional<Solid> s = stlParser.parse();
 		
-
 		s.ifPresent(b -> {
-			try {
+			try
+			{
 				initialiseFwdSynchroniser();
-
+				
 				sync.getSourceResource().getContents().add(b);
-				//System.out.println(sync.getSourceResource().getContents());
+				// System.out.println(sync.getSourceResource().getContents());
 				
 				sync.forward();
-				//System.out.println(sync.getTargetResource().getContents());
+				// System.out.println(sync.getTargetResource().getContents());
 				sync.saveModels();
 				System.out.println(sync.getTargetResource().getContents());
 				Metamodell.Solid solid = (Metamodell.Solid) sync.getTargetResource().getContents().get(0);
-			
+				
 				System.out.println(solid.getFacets().get(0).getEdges().get(0).getP());
 				System.out.println(solid.getFacets().get(0).getEdges().get(0).getF());
 				System.out.println(solid.getFacets().get(0).getEdges().get(1).getP());
 				System.out.println(solid.getFacets().get(0).getEdges().get(1).getF());
 				System.out.println(solid.getFacets().get(0).getEdges().get(2).getP());
 				System.out.println(solid.getFacets().get(0).getEdges().get(2).getF());
-				System.out.println("-----------------------");
+				System.out.println("-----------------------------------------------");
 				System.out.println(solid.getFacets().get(1).getEdges().get(0).getP());
 				System.out.println(solid.getFacets().get(1).getEdges().get(0).getF());
 				System.out.println(solid.getFacets().get(1).getEdges().get(1).getP());
@@ -81,13 +88,22 @@ public class Parser {
 				System.out.println(solid.getFacets().get(1).getEdges().get(2).getP());
 				System.out.println(solid.getFacets().get(1).getEdges().get(2).getF());
 				
-				
-			
-			
-				
-		
-				
-			} catch (IOException e) {
+				Controller ctrl = new Controller();
+				System.out.println("<computeDegree>");
+				for (Facet face : solid.getFacets())
+				{
+					for (Edge e : face.getEdges())
+					{
+						ctrl.computeDegree(e);
+					}
+				}
+				System.out.println("</computeDegree>");
+				System.out.println("<createAreas>");
+//				ctrl.createAreas(solid);
+				System.out.println("</createAreas>");
+			}
+			catch (IOException e)
+			{
 				e.printStackTrace();
 			}
 		});
